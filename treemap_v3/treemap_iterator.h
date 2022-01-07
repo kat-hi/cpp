f316fc2e0cf71c4068bf0a7dd753b3460a3395cf
katharina <katharina.sachs@seerene.com>
2022-01-07 12:09:36 +0100

    iterator constructor

--- treemap_v3/treemap_iterator.h
+++ treemap_v3/treemap_iterator.h
@@ -9,7 +9,7 @@ namespace my {
 
     // iterator: references a node within the tree
     template<typename K, typename T>
-    class treemap<K,T>::iterator {
+    class treemap<K, T>::iterator {
     protected:
 
         // treemap is a friend, can call protected constructor
@@ -17,8 +17,8 @@ namespace my {
 
         // construct iterator referencing a speciic node
         // - only treemap shall be allowed to do so
-        iterator(/* todo */)
-        {}
+        iterator(const std::shared_ptr <node> &val, const std::shared_ptr <node> &root) : nodeObserver_(
+                std::weak_ptr<node>(val)), root_(std::weak_ptr<node>(root)) {}
 
         // non-owning reference to the actual node
         /* todo */
@@ -26,38 +26,37 @@ namespace my {
     public:
 
         // access data of referenced map element (node)
-        value_type& operator*()
-        {
-            /* todo */ static value_type dummy; return dummy;
+        value_type &operator*() {
+            /* todo */ static value_type dummy;
+            return dummy;
         }
-        value_type* operator->()
-        {
-            /* todo */ static value_type dummy; return &dummy;
+
+        value_type *operator->() {
+            /* todo */ static value_type dummy;
+            return &dummy;
         }
 
         // two iterators are equal if they point to the same node
-        bool operator==(const iterator&) const
-        {
+        bool operator==(const iterator &) const {
             /* todo */ return false;
         }
 
-        bool operator!=(const iterator&) const
-        {
+        bool operator!=(const iterator &) const {
             /* todo */ return false;
         }
 
         // next element in map, pre-increment
         // note: must modify self!
-        iterator& operator++()
-        {
-            /* todo */ static iterator dummy; return dummy;
+        iterator &operator++() {
+            /* todo */ static iterator dummy;
+            return dummy;
         }
 
         // prev element in map, pre-decrement
         // note: must modify self!
-        iterator& operator--()
-        {
-            /* todo */ static iterator dummy; return dummy;
+        iterator &operator--() {
+            /* todo */ static iterator dummy;
+            return dummy;
         }
 
     }; // class iterator
