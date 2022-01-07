a46206dc5774757e27c52891873981f9aed8000d
katharina <katharina.sachs@seerene.com>
2022-01-07 12:29:56 +0100

    treemap node

--- treemap_v3/treemap_node.h
+++ treemap_v3/treemap_node.h
@@ -15,7 +15,14 @@ namespace my {
             : public std::enable_shared_from_this<node> {
 
     public:
+        std::weak_ptr<node> parent_ = std::weak_ptr<node>();
+        std::shared_ptr<node> child_L_;
+        std::shared_ptr<node> child_R_;
+        std::pair<K, T> data_ = std::pair<K, T>();
 
+        node() : child_L_(nullptr), child_R_(nullptr) {};
+
+        node(const std::pair<K, T> &data) : data_(data), child_L_(nullptr), child_R_(nullptr) {}
     };
 
 
